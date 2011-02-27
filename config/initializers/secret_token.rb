76c238616446426da7b426a2a49c3e7f173d6b95
Kevin Flick <pooroldflick@gmail.com>
2011-02-27 06:49:26 -0800

    Adding to Github

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Followflick::Application.config.secret_token = '5c648d85ef3e8cdafd56fc737351f3c0a0b611223c02ed7b6155e6d07bf1149c79b3c6872cbc11c3d46a3af6319b63d8ee982f97f731560a7f57f7dfa5bc3dfa'
